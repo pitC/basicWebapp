e48e591b98b1e3be0d373db888538e07bfeb0ce3
pitC <p.k.cabaj@gmail.com>
2018-09-25 22:06:40 +0200

    trying to get vue components working...

--- public/js/vueapp.js
+++ public/js/vueapp.js
@@ -1,15 +1,37 @@
-var app = new Vue({
-    el: '#v-table',
-    data: {
+var list_entry = Vue.extend(
+    {
 
-        items: []
+    // methods:{
+    //     delete: function(event){
+    //         console.log("delete "+this.item._id);
+    //     }
+    // },
+    
+    template: '<p>test</p>'
 
+})
+
+Vue.component('list-entry',list_entry)
+
+// <button type="button" class="btn btn-outline-danger btn-lg" v-on:click="delete">
+// <span class="glyphicon glyphicon-remove-circle"></span>
+// </button>
+
+new Vue({
+    el: 'app',
+    data: function () {
+        return {
+            items: [
+                { _id: 1111111111111 },
+                { _id: 2222222222222 }
+            ]
+        }
     },
     mounted() {
         axios
             .get('http://localhost:3000/objects')
             .then(response => {
-                this.items = response.data;
+                // this.items = response.data;
             });
     }
-});
+})
