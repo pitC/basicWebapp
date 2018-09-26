844f62b65584bde1f31122dc9828cc22589fa632
pitC <p.k.cabaj@gmail.com>
2018-09-26 21:30:47 +0200

    Deleting implemented

--- public/js/vueapp.js
+++ public/js/vueapp.js
@@ -1,37 +1,25 @@
-var list_entry = Vue.extend(
-    {
-
-    // methods:{
-    //     delete: function(event){
-    //         console.log("delete "+this.item._id);
-    //     }
-    // },
-    
-    template: '<p>test</p>'
-
-})
-
-Vue.component('list-entry',list_entry)
-
-// <button type="button" class="btn btn-outline-danger btn-lg" v-on:click="delete">
-// <span class="glyphicon glyphicon-remove-circle"></span>
-// </button>
-
 new Vue({
     el: 'app',
-    data: function () {
-        return {
-            items: [
-                { _id: 1111111111111 },
-                { _id: 2222222222222 }
-            ]
-        }
+    data: {
+        items: []
     },
+    methods: {
+        onDelete: function (index) {
+            console.log("delte " + index);
+            var item = this.items[index];
+            axios
+                .delete('http://localhost:3000/objects/' + item._id)
+                .then(response => {
+                    this.$delete(this.items, index)
+                })
+        }
+    }
+    ,
     mounted() {
         axios
             .get('http://localhost:3000/objects')
             .then(response => {
-                // this.items = response.data;
+                this.items = response.data;
             });
     }
 })
